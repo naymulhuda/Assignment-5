32e3752e8f61cc1aabea155be8a8ef8664304b47
Naymul Huda <naymulhudaa@gmail.com>
2024-09-23 17:17:56 +0600

    button color transformation complete

--- js/donation.js
+++ js/donation.js
@@ -11,11 +11,35 @@ const donateBtnOne = document.getElementById('donate-btn-noakhali').addEventList
     const newDonation = balance + noakhaliDonationInput;
 
     document.getElementById('donation-increase').innerText = newDonation;
+
+    const mainBalance = parseFloat(document.getElementById('donation-decrease').innerText);
+
+    const newMainBalance = mainBalance - noakhaliDonationInput;
+    document.getElementById('donation-decrease').innerText = newMainBalance;
    }
    else {
      alert('Invalid Number');
    }
-
+   
    
    
 })
+
+
+// History tab functionality
+
+// const historyTab = document.getElementById('history-btn');
+// historyTab.addEventListener('click', function () {
+
+//   historyTab.classList.add('bg-lime-300');
+// });
+
+const historyTab = document.getElementById('history-btn');
+const donationTab = document.getElementById('donation-btn');
+
+historyTab.addEventListener('click', function () {
+  historyTab.classList.add('bg-[#bef264]' );
+
+  donationTab.classList.remove('bg-lime-300');
+});
+
