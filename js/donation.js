0791d79af8fcc324056d90443ae96fa463cc8237
Naymul Huda <naymulhudaa@gmail.com>
2024-09-23 23:19:40 +0600

    first donation history done

--- js/donation.js
+++ js/donation.js
@@ -1,4 +1,6 @@
-const donateBtnOne = document.getElementById('donate-btn-noakhali').addEventListener('click', function(){
+
+// Noakhali donate btn
+const donateBtnOne = document.getElementById('donate-btn').addEventListener('click', function(){
    const noakhaliDonationInput = parseFloat(document.getElementById('donation-input-noakhali').value);
    console.log(noakhaliDonationInput);
 
@@ -16,12 +18,39 @@ const donateBtnOne = document.getElementById('donate-btn-noakhali').addEventList
 
     const newMainBalance = mainBalance - noakhaliDonationInput;
     document.getElementById('donation-decrease').innerText = newMainBalance;
+    
+    //History
+    const div = document.createElement('div');
+    div.classList.add('h-32', 'w-9/12', 'border-2', 'border-gray-400', 'flex', 'flex-col', 'justify-center', 'p-5', 'rounded-lg')
+    div.innerHTML = `
+
+    <h4 class= "text-xl font-bold mb-4">${newDonation} Taka is donated for flood at Noakhali </h4>
+    <p class= "text-gray-600"> ${new Date().toLocaleDateString()} </p>
+    `
+    document.getElementById('transaction-history').appendChild(div);
+
+
+    // p.innerText = `Added : ${newDonation} Taka is donated for flood at Noakhali`;
+    // console.log(p);
+
+    // document.getElementById('transaction-history').appendChild(p);
    }
    else {
      alert('Invalid Number');
    }
+  //  // clear before history function 
+  //  // noakhali
+  //  const historyItem = document.createElement('div');
+  //  historyItem.className = 'bg-white border-2 border-gray-600 p-4 rounded-md';
+
+  //  historyItem.innerHTML = `
    
-   
+  //     <p class= "text-xs text-gray-500">${new Date().toLocaleDateString()}</p>
+  //     <p class= "text-xs text-gray-500"> Donate: $${newDonation.toFixed(2)}</p>
+  //  `;
+  //  const historyContainer = document.getElementById('transaction-history');
+
+  //  historyContainer.insertBefore(historyItem, historyContainer.firstChild);
    
 })
 
@@ -37,6 +66,13 @@ historyTab.addEventListener('click', function () {
   donationTab.classList.remove('bg-lime-300');
   donationTab.classList.add('text-gray-500');
 
+   
+  // hide donation section
+  document.getElementById('donation-section').classList.add('hidden');
+
+   // add transaction section
+   document.getElementById('transaction-history').classList.remove('hidden');
+  
 });
 
 donationTab.addEventListener('click', function(){
@@ -44,4 +80,10 @@ donationTab.addEventListener('click', function(){
 
   historyTab.classList.remove('bg-[#bef264]')
   historyTab.classList.add('text-gray-500');
+
+  document.getElementById('donation-section').classList.remove('hidden');
+  
+  // remove transaction section
+  document.getElementById('transaction-history').classList.add('hidden');
+  
 })
